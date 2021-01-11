40f6f5e8c4428f110ab0e50b139b3810f1f3a87b
yano-kentaro <ynkn0829@gmail.com>
2021-01-11 18:10:48 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ルートパスへのアクセスがあったら、messagesコントローラーのindexアクションが呼び出されるよう指示
+  root to: "messages#index"
 end
