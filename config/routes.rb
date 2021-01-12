afddebb7fd1b7acae5ab2bcb2af56ed235653776
yano-kentaro <ynkn0829@gmail.com>
2021-01-12 19:57:17 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ルートパスへのアクセスがあったら、messagesコントローラーのindexアクションが呼び出されるよう指示
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
