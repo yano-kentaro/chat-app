90f9a41455b23167e64de098af4109ffed578129
yano-kentaro <ynkn0829@gmail.com>
2021-01-12 14:50:14 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # ルートパスへのアクセスがあったら、messagesコントローラーのindexアクションが呼び出されるよう指示
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
 end
