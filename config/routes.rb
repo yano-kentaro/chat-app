b6f2ad5ded9954cfc602fbb4b739b5c8bc749eb3
yano-kentaro <ynkn0829@gmail.com>
2021-01-13 14:07:12 +0900

    コードの簡略化

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # ルートパスへのアクセスがあったら、messagesコントローラーのindexアクションが呼び出されるよう指示
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
